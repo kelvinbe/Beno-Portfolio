9812cfcd91ae64c10b97b3b19d00b73d4e53d3f3
kelvinbe <kelvin.beno.k@gmail.com>
2023-09-22 14:45:22 +0300

    update on footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
+      <div className="container p-12 flex justify-between justify-center items-center">
         <span>
             <Image
               src="/images/beno.jpg"  
