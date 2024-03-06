f8328c489f868393f12a0c3a0d69e82d55f0fe31
kelvinbe <kelvin.beno.k@gmail.com>
2024-03-06 10:43:39 +0300

    update

--- src/app/components/OctoImage.jsx
+++ src/app/components/OctoImage.jsx
@@ -4,7 +4,7 @@ const ImageComponent = ({ image }) => {
 
   return (
     <div style={{ borderRadius: '50%', overflow: 'hidden', width: '150px', height: '150px' }}>
-      <Image src={image} alt="Octocat" width={150} height={image === '/project.gif' ? 129 : 150} />
+      <Image src={image} alt="Octocat" width={150} height={150} />
     </div>
   );
 };
