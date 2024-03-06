5802031e8f58d332465602986d6426219e97aaf6
kelvinbe <kelvin.beno.k@gmail.com>
2024-03-06 03:22:03 +0300

    updates

--- src/app/components/OctoImage.jsx
+++ src/app/components/OctoImage.jsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
 
-const ImageComponent = ({image}) => {
-  return <Image src={image} alt="Octocat" width={150} height={300} />;
+const ImageComponent = ({ image }) => {
+
+  return (
+    <div style={{ borderRadius: '50%', overflow: 'hidden', width: '150px', height: '150px' }}>
+      <Image src={image} alt="Octocat" width={150} height={image === '/project.gif' ? 129 : 150} />
+    </div>
+  );
 };
 
 export default ImageComponent;
